9773d4c0d3bda11003a84885f8d351a0a9676fb1
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2020-09-18 17:40:44 +0400

    initial commit

---
+++ NxWorkspaces/shopping-system/future/apps/kamazon/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'future-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'kamazon';
+}
